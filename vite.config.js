dd99fc5dec09b90bf70f4d51237134e0bb3a9b55
hamzabatayneh0y0 <hamzabatyneh@gmail.com>
2025-03-19 15:48:36 +0300

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/MediTro-Template-Practicing",
+  base: "/MediTro-Template-Practicing/",
   plugins: [react()],
 });
