561cdce3adfda432b9f2154daabf17d47a1a9cf9
hamzabatayneh0y0 <hamzabatyneh@gmail.com>
2025-03-19 16:32:04 +0300

    h

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/MediTro-Template-Practicing",
+  base: "/MediTro-Template-Practicing/",
 
   plugins: [react()],
 });
