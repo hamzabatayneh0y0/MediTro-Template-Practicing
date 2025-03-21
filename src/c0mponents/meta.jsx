0f12d86237eec0752578cfe4adf2509c4aad0e6d
hamzabatayneh0y0 <hamzabatyneh@gmail.com>
2025-03-21 14:22:56 +0300

    final

--- src/c0mponents/meta.jsx
+++ src/c0mponents/meta.jsx
@@ -3,25 +3,28 @@ import { Helmet } from "react-helmet";
 const MetaTags = ({title}) => {
   return (
     <Helmet>
+      <title>{title}</title>
+
       {/* 🔹 Open Graph (Facebook, WhatsApp) */}
       <meta property="og:title" content="MediTro-practicing" />
-      <meta property="og:description" content="MediTro template redeigning practicing" />
-      <meta property="og:image" content="https://raw.githubusercontent.com/hamzabatayneh0y0/MediTro-Template-Practicing/main/public/4552998.png" />
-      <meta property="og:url" content="https://github.com/hamzabatayneh0y0/MediTro-Template-Practicing/" />
+      <meta property="og:description" content="MediTro template redesigning practicing" />
+      <meta property="og:image" content="https://meditro-template-practicing.netlify.app/4552998.png" />
+      <meta property="og:url" content="https://meditro-template-practicing.netlify.app/" />
       <meta property="og:type" content="website" />
 
       {/* 🔹 Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="MediTro-practicing" />
-      <meta name="twitter:description" content="MediTro template redeigning practicing" />
-      <meta name="twitter:image" content="https://raw.githubusercontent.com/hamzabatayneh0y0/MediTro-Template-Practicing/main/public/4552998.png" />
+      <meta name="twitter:description" content="MediTro template redesigning practicing" />
+      <meta name="twitter:image" content="https://meditro-template-practicing.netlify.app/4552998.png" />
 
       {/* 🔹 Meta Tags for SEO */}
       <meta name="description" content="MediTro template redesigning practicing" />
-      <meta name="keywords" content="meditro, practicing, health, healthcare" />
+      <meta name="keywords" content="meditro, practicing, health, healthcare ,React template" />
       <meta name="author" content="hamza" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="robots" content="index, follow"/>
 
-      <title>{title}</title>
     </Helmet>
   );
 };
